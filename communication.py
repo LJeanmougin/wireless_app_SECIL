6edb14f81ac44a26cd08300fa22b6244685fb710
Louison Jeanmougin <louis.jeanmougin@gmail.com>
2022-03-10 19:23:15 +0100

    communication fix

--- communication.py
+++ communication.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python3
+
 import sys
 from wireless_listener import *
 from wireless_talker import *
 
 if __name__ == "__main__":
     
-    if len(sys.argv) != 3 :
-        print("Usage" + sys.argv[0] + ": protocol send/receive")
+    if len(sys.argv) != 4 :
+        print("Usage" + sys.argv[0] + ": protocol send/receive config_file")
         exit(1)
     protocol = sys.argv[1]
     role = sys.argv[2]
+    config = sys.argv[3]
     if protocol == "bluetooth":
         protocol = BLUETOOTH
     elif protocol == "wifi":
@@ -17,9 +20,9 @@ if __name__ == "__main__":
         print("Protocole inconnu (bluetooth/wifi)")
         exit(2)
     if role == "send":
-        device = WirelessTalker(protocol)
+        device = WirelessTalker(protocol, config)
     elif role == "receive":
-        device = WirelessListener(protocol)
+        device = WirelessListener(protocol, config)
     else :
         print("Role inconnu (send/receive)")
         exit(3)
